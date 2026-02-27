f18483a93335b84fe8c20269da170ab84cd7f099
ng845915-droid <ng845915@gmail.com>
2026-02-27 16:03:22 -0600

    Add files via upload

---
+++ js/historia.js
@@ -0,0 +1,54 @@
+
+
+const history = [
+  {
+    year: "1996-08",
+    title: "Un club centenario",
+    description: "Los actos conmemorativos fueron una fiesta de barcelonismo, un recorrido emotivo por la historia del Club que se celebró durante todo el año.",
+    image:"assets/img1.png"
+  },
+  {
+    year: "1988-96",
+    title: "La era del 'Dream Team'",
+    description: "El FC Barcelona gana su primera Copa de Europa en 1992.",
+    image: "assets/img2.png"
+  },
+  {
+    year: "1978-88",
+    title: "Más socios, más estrellas",
+    description: "Llegan futbolistas de ensueño como Maradona, Schuster o Quini, y surgen los primeros contratos multimillonarios y los derechos de televisión.",
+    image: "assets/img3.png"
+  },
+  {
+    year: "1969-78",
+    title: "Cruyff, democracia",
+    description: "Con Montal como presidente, el sueño azulgrana de fichar a Johan Cruyff empezaba a ser posible.",
+    image: "assets/img4.png"
+  },
+  {
+    year: "1961-69",
+    title: "Nueva dimensión social",
+    description: "El Club incrementa el número de socios a pesar de los malos resultados del equipo en los años sesenta.",
+    image: "assets/imgg5.png"
+  },
+  {
+    year: "1950-61",
+    title: "Kubala y su tiempo",
+    description:"El Club dobla su masa social y se construye el Camp Nou, inaugurado el 24 de septiembre de 1957 durante la presidencia de Francesc Miró-Sans.",
+    image: "assets/img6.png"
+  },
+  {
+    year: "1939-50",
+    title: "Años de perseverancia",
+   description: "La dura represión y la depuración llevada a cabo por las autoridades militares y civiles en los años de la posguerra desnaturalizó la entidad.",
+    image: "assets/img7.png"
+  },
+  {
+    year: "1930-39",
+    title: "Luchando contra la historia",
+    description: "La década de los treinta estuvo marcada por una inestabilidad política y una crisis general que inevitablemente afectó al FC Barcelona.",
+    image: "assets/img8.png"
+  }
+];
+
+window.clubHistory = history;
---
+++ js/players.js
@@ -0,0 +1,20 @@
+const players = [
+  { id: 1, name: "Joan Garcia", position: "Portero" },
+  { id: 2, name: "Iñaki Peña", position: "Portero" },
+  { id: 3, name: "Ronald Araújo", position: "Defensa central" },
+  { id: 4, name: "Jules Koundé", position: "Defensa central / Lateral derecho" },
+  { id: 5, name: "Andreas Christensen", position: "Defensa central" },
+  { id: 6, name: "Alejandro Balde", position: "Lateral izquierdo" },
+  { id: 7, name: "João Cancelo", position: "Lateral derecho / izquierdo" },
+  { id: 8, name: "Frenkie de Jong", position: "Centrocampista" },
+  { id: 9, name: "Pedri", position: "Centrocampista" },
+  { id: 10, name: "Gavi", position: "Centrocampista" },
+  { id: 11, name: "Dani Olmo", position: "Centrocampista" },
+  { id: 12, name: "Fermin Lopez", position: "Mediocentro defensivo" },
+  { id: 13, name: "Robert Lewandowski", position: "Delantero centro" },
+  { id: 14, name: "Raphinha", position: "Extremo derecho" },
+  { id: 15, name: "Ferran Torres", position: "Extremo / Delantero" },
+  { id: 16, name: "Lamine Yamal", position: "Extremo derecho" },
+];
+
+window.FCBplayers = players;
